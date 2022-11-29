7200d2852941e0d7ea53e549c31fa7bb63a4ea82
Ryan Savill <65285466+Cryaaa@users.noreply.github.com>
2022-11-29 15:59:15 +0100

    new pca embedding function

--- organoid_prediction_python/data_processing/__init__.py
+++ organoid_prediction_python/data_processing/__init__.py
@@ -5,4 +5,4 @@ from ._feature_processing import (
     reform_cellprofiler_table,
 )
 from ._graph_distance_measurement import calculate_distance_matrix
-from ._dimension_reduction import umap_with_indices_and_ground_truth
+from ._dimension_reduction import umap_with_indices_and_ground_truth, PCA_with_indices_and_ground_truth
--- organoid_prediction_python/data_processing/_dimension_reduction.py
+++ organoid_prediction_python/data_processing/_dimension_reduction.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import umap
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from .._utils import _try_dropping
 
 # TODO docstring
@@ -30,9 +31,36 @@ def umap_with_indices_and_ground_truth(
         index=index,
         columns=[f"UMAP_{i+1}" for i in range(n_components)],
     )
+    if ground_truth_df is None:
+        return umap_df
+    return pd.concat([umap_df,ground_truth_df[gt_keys]],axis=1).dropna()
 
-    return pd.concat([umap_df,ground_truth_df[gt_keys]],axis=1)
+def PCA_with_indices_and_ground_truth(
+    dataframe: pd.DataFrame,
+    ground_truth_df: pd.DataFrame = None,
+    gt_keys:list = ["Axes","Morph","Morph_Class"],
+    n_components=2,
+    remove_unclassified = True,
+):
+    if remove_unclassified:
+        dataframe = dataframe.loc[(ground_truth_df["Morph"]!="unclassified")&(ground_truth_df["Axes"]!="unclassified")]
+        if ground_truth_df is not None:
+            ground_truth_df = ground_truth_df.loc[(ground_truth_df["Morph"]!="unclassified")&(ground_truth_df["Axes"]!="unclassified")]
 
+    
+    index = dataframe.index
+    data = _try_dropping(dataframe)
 
+    transformer = PCA(n_components=n_components,)
+    embedding = transformer.fit_transform(data)
+    pca_df = pd.DataFrame(
+        embedding,
+        index=index,
+        columns=[f"PC_{i+1}" for i in range(n_components)],
+    )
+
+    if ground_truth_df is None:
+        return pca_df
+    return pd.concat([pca_df,ground_truth_df[gt_keys]],axis=1).dropna()
 
     
