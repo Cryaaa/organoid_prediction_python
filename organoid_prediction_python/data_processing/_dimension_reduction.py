61b01faba16747279901066ffae8489af5d85510
Ryan Savill <65285466+Cryaaa@users.noreply.github.com>
2023-01-05 09:45:30 +0100

    adding docstrings

--- organoid_prediction_python/data_processing/_dimension_reduction.py
+++ organoid_prediction_python/data_processing/_dimension_reduction.py
@@ -5,16 +5,42 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from .._utils import _try_dropping
 
-# TODO docstring
 def umap_with_indices_and_ground_truth(
     dataframe: pd.DataFrame,
     ground_truth_df: pd.DataFrame = None,
-    gt_keys:list = ["Axes","Morph","Morph_Class"],
-    standardscale = True,
-    n_neighbors=10,
-    n_components=2,
-    random_state = None,
+    gt_keys: list = ["Axes","Morph","Morph_Class"],
+    standardscale: bool = True,
+    n_neighbors: int = 10,
+    n_components: int = 2,
+    random_state: int = None,
 ):
+    """
+    Generates a UMAP from a dataframe and returns it as a dataframe
+    containing the grounbd truth specified by ground_truth_df. Both 
+    dataframes need to have matching indices.
+
+    Parameters
+    ----------
+
+    dataframe: pd.DataFrame
+        Input dataframe from which the UMAP is generated.
+    ground_truth_df: pd.Dataframe
+        Dataframe containing the ground truth that will be added to the
+        output dataframe
+    gt_keys: list
+        List of strings specifying which columns from the groound_truth_df
+        will be added to the output dataframe.
+    standardscale: bool
+        If True standardscaling / z-normalisation will be performed on the 
+        data prior to UMAP generation
+    n_neighbors: int
+        Parameter specifying the number 0of neighbors to regard during 
+        UMAP generation (see UMAP documentation)
+    n_components: int
+        Number of dimensions the UMAP will have.
+    random_state: int
+        Random seed which can be used to get the same result twice
+    """
     index = dataframe.index
     data = _try_dropping(dataframe)
     if standardscale:
@@ -35,6 +61,7 @@ def umap_with_indices_and_ground_truth(
         return umap_df
     return pd.concat([umap_df,ground_truth_df[gt_keys]],axis=1).dropna()    
 
+# TODO docstring
 def PCA_with_indices_and_ground_truth(
     dataframe: pd.DataFrame,
     ground_truth_df: pd.DataFrame = None,
